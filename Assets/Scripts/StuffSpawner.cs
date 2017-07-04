4080771534b6bc6b3964b38ffbdc667047ec0472
Davy Peter Braun <davy.braun@callr.com>
2017-07-04 17:14:11 +0200

    Finish demo

--- Assets/Scripts/StuffSpawner.cs
+++ Assets/Scripts/StuffSpawner.cs
@@ -2,17 +2,20 @@ using UnityEngine;
 
 public class StuffSpawner : MonoBehaviour
 {
-    public float timeBetweenSpawns;
     public Stuff[] stuffPrefabs;
     public float velocity;
+    public FloatRange timeBetweenSpawns, scale, randomVelocity, angularVelocity;
+    public Material stuffMaterial;
 
     float timeSinceLastSpawn;
+    float currentSpawnDelay;
 
     void FixedUpdate ()
     {
         timeSinceLastSpawn += Time.deltaTime;
-        if (timeSinceLastSpawn >= timeBetweenSpawns) {
-            timeSinceLastSpawn -= timeBetweenSpawns;
+        if (timeSinceLastSpawn >= currentSpawnDelay) {
+            timeSinceLastSpawn -= currentSpawnDelay;
+            currentSpawnDelay = timeBetweenSpawns.RandomInRange;
             SpawnStuff();
         }
     }
@@ -21,7 +24,13 @@ public class StuffSpawner : MonoBehaviour
     {
         Stuff prefab = stuffPrefabs[Random.Range(0, stuffPrefabs.Length)];
         Stuff spawn = Instantiate<Stuff>(prefab);
+
         spawn.transform.localPosition = transform.position;
-        spawn.Body.velocity = transform.up * velocity;
+        spawn.transform.localScale = Vector3.one * scale.RandomInRange;
+        spawn.transform.localRotation = Random.rotation;
+
+        spawn.Body.velocity = transform.up * velocity + Random.onUnitSphere * randomVelocity.RandomInRange;
+        spawn.Body.angularVelocity = Random.onUnitSphere * angularVelocity.RandomInRange;
+        spawn.GetComponent<MeshRenderer>().material = stuffMaterial;
     }
 }
