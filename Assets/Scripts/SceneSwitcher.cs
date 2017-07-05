7c16699f3b66f4a03fd05def4b4838da8c81ac75
Davy Peter Braun <davy.braun@callr.com>
2017-07-05 10:22:55 +0200

    Scene switching ability

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void SwitchScene () 
+    {
+        int nextLevel = (SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextLevel);
+    }
+}
